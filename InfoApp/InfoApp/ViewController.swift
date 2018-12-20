37c64b598c39448bd78323cfd911bd14a8799186
Sudarshan Shetty <>
2018-12-20 11:30:14 +0530

    Initial setup

---
+++ InfoApp/InfoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfoApp
+//
+//  Created by Sudarshan Shetty on 20/12/18.
+//  Copyright © 2018 SAP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
